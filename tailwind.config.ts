2ab9aa77d43ac5c74d8f6a553c05461de7fb1a81
Daniel Magro <dando385@yahoo.com>
2024-03-02 01:37:59 -0500

    fixed src directory and rearranged files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,11 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  daisyui: {
+    themes: ["nord"],
+  },
+  plugins: [
+    require('daisyui'),
+  ],
 };
 export default config;
