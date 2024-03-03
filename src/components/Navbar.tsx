e86470e70fca1bd3c4efacce53e50509b96b51ce
Daniel Magro <dando385@yahoo.com>
2024-03-03 02:02:09 -0500

    added penn state photos

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,51 @@
 // src/components/Navbar.tsx
+import React, { useState } from 'react';
 import Link from 'next/link';
 
 const Navbar = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   return (
-    <div className="navbar bg-green-400 text-slate-700">
-      <div className="navbar-start">
-        <span className="text-lg font-bold uppercase" style={{ fontFamily: 'Oswald, sans-serif' }}>Dan Magro Blockchain Developer</span>
+    <div className="flex flex-col lg:flex-row lg:items-center bg-green-400 text-slate-700">
+      <div className="flex justify-between items-center p-4 lg:p-0">
+        <span className="text-lg font-bold uppercase" style={{ fontFamily: 'Oswald, sans-serif' }}>
+          Dan Magro Blockchain Developer
+        </span>
+        <button
+          onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+          className="lg:hidden p-2 text-slate-700"
+        >
+          {isSidebarOpen ? 'Close' : 'Menu'}
+        </button>
       </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal p-0">
-          <li><Link href="/blog">Blog</Link></li>
-          <li><Link href="/certificates">Certificates</Link></li>
-          <li><Link href="/education">Education</Link></li>
-          <li><Link href="/portfolio">Portfolio</Link></li>
-          <li><Link href="/resume">Resume</Link></li>
+      <div className={`lg:flex ${isSidebarOpen ? 'flex' : 'hidden'} flex-col lg:flex-row lg:items-center`}>
+        <ul className="menu menu-horizontal lg:menu-vertical p-0">
+          <li>
+            <Link href="/blog">
+              Blog
+            </Link>
+          </li>
+          <li>
+            <Link href="/certificates">
+              Certificates
+            </Link>
+          </li>
+          <li>
+            <Link href="/education">
+              Education
+            </Link>
+          </li>
+          <li>
+            <Link href="/portfolio">
+              Portfolio
+            </Link>
+          </li>
+          <li>
+            <Link href="/resume">
+              Resume
+            </Link>
+          </li>
         </ul>
-      </div>
-      {/* Navbar End for potential future use */}
-      <div className="navbar-end">
-        {/* Additional elements can go here */}
       </div>
     </div>
   );
--- src/pages/education.tsx
+++ src/pages/education.tsx
@@ -1,4 +1,8 @@
 import Navbar from "@/components/Navbar";
+import Image from 'next/image';
+import { useRef, useState, useEffect } from 'react';
+import SwiperCore, { Navigation, Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'react-responsive-carousel';gacp 
 
 // pages/education.tsx
 const Education = () => {
