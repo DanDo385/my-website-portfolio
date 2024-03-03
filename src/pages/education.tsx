eafd647019c2f7c420589b8298e306dea704a1ef
Daniel Magro <dando385@yahoo.com>
2024-03-03 06:21:36 -0500

    typo on education.tsx in page

--- src/pages/education.tsx
+++ src/pages/education.tsx
@@ -1,31 +1,16 @@
-import Image from 'next/image';
-import Navbar from "@/components/Navbar";
-import Link from 'next/link';
+import PennStateInfo from '../components/PennStateInfo';
+import Navbar from '../components/Navbar';
 
-const Blog = () => {
- return (
- <>
-  <Navbar />
-  <div className="bg-education-background bg-cover bg-center h-screen flex justify-center items-center">
-   <Link href="https://www.c2defi.com" passHref>
-    <Link target="_blank" rel="noopener noreferrer" className="relative inline-block" style={{ width: '400px', height: '400px' }} href={''}>
-     <Image 
-      src="/blog.jpg" 
-      alt="C2DeFi Zone" 
-      width={400} 
-      height={400} 
-      objectFit="cover" 
-      quality={100} 
-      className="absolute"
-     />
-     <div className="absolute inset-0 flex justify-center items-center bg-black bg-opacity-40 z-10">
-      <p className="text-white text-xl font-bold">C2DeFi Zone</p>
-     </div>
-    </Link>
-   </Link>
-  </div>
- </>
- );
-};
 
-export default Blog;
+const education = () => {
+    return (
+        <>
+            <Navbar />
+                <div className="bg-education-background bg-cover bg-center h-screen">
+                    <PennStateInfo />
+                </div>
+        </>
+    )
+}
+
+export default education;
